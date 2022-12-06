92a4d8111094d9365255720e78553a39d3577f8f
muhammadtalha242 <m.talha2244@gmail.com>
2022-12-07 03:47:56 +0500

    generics added

--- custom-useState/useState.ts
+++ custom-useState/useState.ts
@@ -1,11 +1,9 @@
-const useState = (
-  initialValue: string
-): [() => string, (value: string) => string] => {
-  let str: string = initialValue;
+function useState<T>(initialValue: T): [() => T, (value: T) => T] {
+  let str: T = initialValue;
   return [() => str, (val) => (str = val)];
-};
+}
 
-const [getValue, setValue] = useState("initial Value");
+const [getValue, setValue] = useState<string> ("initial Value");
 console.log("state before update: ", getValue());
 setValue("Final value");
 console.log("state after update: ", getValue());
